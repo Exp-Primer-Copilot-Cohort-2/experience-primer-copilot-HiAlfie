c5672cbd99ec498f18c0053e2030b7d180b9ac1f
Alfie <66980044+HiAlfie@users.noreply.github.com>
2024-09-23 10:16:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var skills = [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+        'Python',
+        'Django',
+        'Node.js',
+        'Express.js',
+        'MongoDB'
+    ];
+    return skills;
+}
